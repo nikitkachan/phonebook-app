e95911a483949fd80aab6b11ef510d00c0fd5329
nikitkachan <nikyc777@gmail.com>
2024-03-14 19:06:42 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const theme = createTheme();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+    <BrowserRouter basename="/phonebook-app">
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <ThemeProvider theme={theme}>
